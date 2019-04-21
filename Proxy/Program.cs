8c0f2a43592c3acac9c19b1a0adf36d12918778d
Kartik Chandra Biswas <onsaurav@hotmail.com>
2019-04-21 20:51:44 +0600

    Initial commit

---
+++ Proxy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy proxy = new Proxy();
+            proxy.Request();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
